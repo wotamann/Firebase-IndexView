37cf90cec75a240cc9e68aec0343c9358810265b
wotamann <woko@aon.at>
2015-03-22 14:52:22 +0100

    JS Hinting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*jshint undef: false, maxerr: 255*/
+/*jshint undef: false, maxerr: 512*/
 /*
      Firebase IndexView - Demo Application
      @version 0.1.0
@@ -17,68 +17,79 @@ app.controller('appCtrl', ['$scope', 'FirebaseIndexView', '$timeout', function (
     //---  DINOSAUR  --------------------------------------------------------------------------------------------------------
     
     var indexDeclarationDinosaur = [
-         {   
+        {
             viewName: 'order',
-            mapView: function(emit, doc) {
-                var key=doc.order ? doc.order.toLowerCase() : '',
-                    val=(doc.order && doc.weight) ?  doc.order + ' has weight of ' + doc.weight : "no Dinosaur";                
-                emit(key,val);
+            mapView: function (emit, doc) {
+                var key = doc.order ? doc.order.toLowerCase() : '',
+                    val = (doc.order && doc.weight) ?  doc.order + ' has weight of ' + doc.weight : "no Dinosaur";
+                emit(key, val);
             }
         },
-         {   
+        {
             viewName: 'volume',
-             mapView: function(emit, doc) {
-                var key=doc.length && doc.weight ? (doc.length * doc.weight ) : null,
-                    val=(doc.length && doc.weight) ?  doc._key + ' has a volume from  ' + (doc.length * doc.weight  ) : "no volume"    
-                emit(key,val);
-            }  
+            mapView: function (emit, doc) {
+                var key = doc.length && doc.weight ? (doc.length * doc.weight) : null,
+                    val = (doc.length && doc.weight) ?  doc._key + ' has a volume from  ' + (doc.length * doc.weight) : "no volume";
+                emit(key, val);
+            }
         },
-        {   
-            viewName: 'timespan',            
-            mapView: function(emit, doc) {
-                var key=doc.appeared && doc.vanished ? (doc.vanished-doc.appeared ) : null,
-                    val=(doc.appeared && doc.vanished) ?  doc._key + ' lives ' + (doc.vanished-doc.appeared  ) + ' years.': "no timespan";     
-                emit(key,val);
-            }   
+        {
+            viewName: 'timespan',
+            mapView: function (emit, doc) {
+                var key = doc.appeared && doc.vanished ? (doc.vanished - doc.appeared) : null,
+                    val = (doc.appeared && doc.vanished) ?  doc._key + ' lives ' + (doc.vanished - doc.appeared) + ' years.' : "no timespan";
+                emit(key, val);
+            }
         },
-        {   
+        {
             viewName: 'isFlying',
-            mapView: function(emit, doc) {
-                if ((doc.order==='ornithischia')===true) emit(doc._key,true);
-            }            
-        },
+            mapView: function (emit, doc) {
+                if ((doc.order === 'ornithischia') === true) {
+                    emit(doc._key, true);
+                }
+            }
+        }
   
     ];
     
     var referenceDinosaur = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
     var referenceDinosaurIDX = new Firebase("https://fireindex.firebaseio.com/IDX-dinosaurs");
-    var IndexViewDino = FirebaseIndexView(referenceDinosaur, indexDeclarationDinosaur, referenceDinosaurIDX); // take Default referenceIndexView
+    var IndexViewDino = new FirebaseIndexView(referenceDinosaur, indexDeclarationDinosaur, referenceDinosaurIDX); // take Default referenceIndexView
 
     IndexViewDino.indexOn();
         
     //--- DINO MEALS --------------------------------------------------------------------------------------------------------
     
     // mapping and emiting function
-    function meals (emit, doc, fullMeal) {
+    function meals(emit, doc, fullMeal) {
         
-        var meals=doc.meals || {};
+        var e,
+            meal = doc.meals || {};
         
-        for(var e in meals) {  // iterate over all meals  drink, food, meat...
-            var n=meals[e];
-            n = n ? n.toLowerCase() : null   
+        for (e in meal) {  // iterate over all meals  drink, food, meat...
+            var n = meal[e];
+            n = n ? n.toLowerCase() : null;
             // iterate and generate an index for each split word in every meal like water, milk, ... in drink 
-            n.split(",").forEach( function(key){ 
-                key=key.trim()+"-"+e;
-                           // emit calculated key and value to build the index
-                fullMeal ? emit(key, meals) : emit(key, doc._linked +" likes following " + e +": "+ meals[e]); 
-            })            
-        }        
-    };  
+            n.split(",").forEach(
+                function (key) {
+                    key = key.trim() + "-" + e;
+
+                    // emit calculated key and value to build the index
+                    if (fullMeal === true) {
+                        emit(key, meal);
+                    } else {
+                        var val = doc._linked + " likes following " + e + ": " + meal[e];
+                        emit(key, val);
+                    }
+                }
+            );
+        }
+    }  
        
     var indexDeclarationDinomeals = [
         {
             viewName: 'dino',
-            mapView: function (emit, doc) { emit(doc._linked, true) }
+            mapView: function (emit, doc) { emit(doc._linked, true); }
         },
         
         {
@@ -95,25 +106,26 @@ app.controller('appCtrl', ['$scope', 'FirebaseIndexView', '$timeout', function (
             viewName: 'mealsPerDay',
             mapView:function(emit, doc) {
                 
-                var key=doc.mealsPerDay || null,
-                    ext_doc=IndexViewDino.getLinkedDocument(doc._linked),
-                    val=doc.mealsPerDay + " Meals per day" + (ext_doc ? " makes a Dino weight of "+ext_doc.weight : " but no Dino");
+                var key = doc.mealsPerDay || null,
+                    ext_doc = IndexViewDino.getLinkedDocument(doc._linked),
+                    val = doc.mealsPerDay + " Meals per day" + (ext_doc ? " makes a Dino weight of " + ext_doc.weight : " but no Dino");
                 
-                emit(key,val);
-            }   
+                emit(key, val);
+            }
 
         },
         
-        {   
+        {
             viewName: 'dinoWithMeal',
-            mapView:function(emit, doc) {
+            mapView: function (emit, doc) {
                 
-                var key=doc._linked || null,
-                    ext_doc=IndexViewDino.getLinkedDocument(doc._linked);
-                    if( ext_doc) {ext_doc.food=doc}; 
-                    var val= ext_doc || 'no dino found';
-                emit(key,val);
-            }   
+                var val,
+                    key = doc._linked || null,
+                    ext_doc = IndexViewDino.getLinkedDocument(doc._linked);
+                if (ext_doc) {ext_doc.food = doc; }
+                val = ext_doc || 'no dino found';
+                emit(key, val);
+            }
           
         }
         
@@ -126,38 +138,51 @@ app.controller('appCtrl', ['$scope', 'FirebaseIndexView', '$timeout', function (
     var referenceDinoMeal = new Firebase(firebaseReferenceDinoMeal);
   
     //  Create an IndexView Object for working on  
-    var IndexViewDinoMeals = FirebaseIndexView(referenceDinoMeal, indexDeclarationDinomeals); // take Default referenceIndexView
+    var IndexViewDinoMeals = new FirebaseIndexView(referenceDinoMeal, indexDeclarationDinomeals); // take Default referenceIndexView
 
     // Let INDEXVIEW automatic working...
-    IndexViewDinoMeals.indexOn();  
+    IndexViewDinoMeals.indexOn();
    
     //--- UI --------------------------------------------------------------------------------------------------------
     
-    $scope.selection = uniqueArray(indexDeclarationDinomeals.concat(indexDeclarationDinosaur)); 
+    $scope.selection = uniqueArray(indexDeclarationDinomeals.concat(indexDeclarationDinosaur));
 
     $scope.isIndexing = IndexViewDinoMeals.isIndexing;
 
     // Some Reduce Functions - function.name becomes property in reduce-object e.g. { 'countMealsFN':10 }
     function countMealsFN(previousValue, currentValue, index, array) {
             
-            if (index===1) previousValue={drink:0,meat:0,vegetarian:0};
-            //console.log(index, previousValue,currentValue)
-            if(currentValue.drink) previousValue.drink=previousValue.drink + currentValue.drink.split(",").length
-            if(currentValue.meat) previousValue.meat=previousValue.meat + currentValue.meat.split(",").length
-            if(currentValue.vegetarian) previousValue.vegetarian=previousValue.vegetarian + currentValue.vegetarian.split(",").length
-           
-            return previousValue;
-        
-        };
+        if (index === 1) {
+            previousValue = {drink: 0, meat: 0, vegetarian: 0};
+        }
+        //console.log(index, previousValue,currentValue)
+        if (currentValue.drink) {
+            previousValue.drink = previousValue.drink + currentValue.drink.split(",").length;
+        }
+        if (currentValue.meat) {
+            previousValue.meat = previousValue.meat + currentValue.meat.split(",").length;
+        }
+        if (currentValue.vegetarian) {
+            previousValue.vegetarian = previousValue.vegetarian + currentValue.vegetarian.split(",").length;
+        }
+
+        return previousValue;
+
+    }
     // Some Reduce Functions - function.name becomes property in reduce-object e.g. { 'CountRecords':10 }
-    function CountRecords (previousValue, currentValue, index, array) {
+    function CountRecords(previousValue, currentValue, index, array) {
             
-            if (index===1) previousValue=1;
-            //console.log(index, previousValue,currentValue)
-            if(currentValue) return previousValue+=1;           
-            return previousValue;
+        if (index === 1) {
+            previousValue = 1;
+        }
+        //console.log(index, previousValue,currentValue)
+        if (currentValue) {
+            previousValue += 1; 
+        }
+
+        return previousValue;
         
-        };
+    }
    
     $scope.query = function (term, index, sort, unique, output, reduce) {
 
@@ -213,12 +238,13 @@ app.controller('appCtrl', ['$scope', 'FirebaseIndexView', '$timeout', function (
 
     $scope.generateMeals = function () {
 
-        var i;
-        var l = 1;
+        var i,
+            l = 1,
+            dinoMeal;
 
-        for (i = 0; i < l; i++) {
+        for (i = 0; i < l; i += 1) {
 
-            var dinoMeal = generateDinoMeal();
+            dinoMeal = generateDinoMeal();
             dinoMeal._linked = getRandom(["lambeosaurus", "bruhathkayosaurus", "linhenykus", "stegosaurus", "pterodactyl", "triceratops"]);
             referenceDinoMeal.push(dinoMeal);
             
@@ -235,29 +261,47 @@ app.controller('appCtrl', ['$scope', 'FirebaseIndexView', '$timeout', function (
             ml = {};
 
         var m = ["Beef", "Sausage", "Lamb", "Turkey", "Dino small", "Dino XXLarge", "Fish", "Duck",  "rotten Carcass", "Humans", "Mammoth"];
-        ml.meat = getRandom(m)+",";
+        ml.meat = getRandom(m) + ",";
         
         var v = ["gras", "grape", "pineapple", "Tomatoes", "fruits", "bananas", "trees", "apples", "grain", "leafs", "straw", "corn"];
-        ml.vegetarian = getRandom(v)+",";
+        ml.vegetarian = getRandom(v) + ",";
         
-        var dx = ["Water", "Milk", "blood", "Beer", "seawater", "Budweiser", "Fruit Juice", "Murauer", "Gösser", "Puntigamer", "Coke", "Cola", "Fanta","Ice Tea"];
-        ml.drink = getRandom(dx)+",";
+        var dx = ["Water", "Milk", "blood", "Beer", "seawater", "Budweiser", "Fruit Juice", "Murauer", "Gösser", "Puntigamer", "Coke", "Cola", "Fanta", "Ice Tea"];
+        ml.drink = getRandom(dx) + ",";
         
-        if (Math.random() > 0.4) ml.meat += getRandom(m)+",";
-        if (Math.random() > 0.4) ml.vegetarian += getRandom(v)+",";
-        if (Math.random() > 0.4) ml.drink += getRandom(dx)+",";
+        if (Math.random() > 0.4) {
+            ml.meat += getRandom(m) + ",";
+        }
+        if (Math.random() > 0.4) {
+            ml.vegetarian += getRandom(v) + ",";
+        }
+        if (Math.random() > 0.4) {
+            ml.drink += getRandom(dx) + ",";
+        }
         
-        if (Math.random() > 0.66) ml.meat += getRandom(m)+",";
-        if (Math.random() > 0.66) ml.vegetarian +=  getRandom(v)+",";
-        if (Math.random() > 0.66) ml.drink += getRandom(dx)+",";
+        if (Math.random() > 0.66) {
+            ml.meat += getRandom(m) + ",";
+        }
+        if (Math.random() > 0.66) {
+            ml.vegetarian +=  getRandom(v) + ",";
+        }
+        if (Math.random() > 0.66) {
+            ml.drink += getRandom(dx) + ",";
+        }
 
-        if (Math.random() > 0.8) ml.meat += getRandom(m)+",";
-        if (Math.random() > 0.8) ml.vegetarian +=  getRandom(v)+",";
-        if (Math.random() > 0.8) ml.drink += getRandom(dx)+",";
+        if (Math.random() > 0.8) {
+            ml.meat += getRandom(m) + ",";
+        }
+        if (Math.random() > 0.8) {
+            ml.vegetarian +=  getRandom(v) + ",";
+        }
+        if (Math.random() > 0.8) {
+            ml.drink += getRandom(dx) + ",";
+        }
 
-        ml.meat=ml.meat.slice(0, -1)
-        ml.drink=ml.drink.slice(0, -1)
-        ml.vegetarian=ml.vegetarian.slice(0, -1)
+        ml.meat = ml.meat.slice(0, -1);
+        ml.drink = ml.drink.slice(0, -1);
+        ml.vegetarian = ml.vegetarian.slice(0, -1);
         
         p.meals = ml;
        
@@ -273,16 +317,18 @@ app.controller('appCtrl', ['$scope', 'FirebaseIndexView', '$timeout', function (
     }
 
     function uniqueArray(source) {
-        var n = {},
+        var i,
+            n = {},
             target = [],
             l = source.length;
-        for (var i = 0; i < l; i++) {
+        
+        for (i = 0; i < l; i += 1) {
             if (!n[source[i].viewName]) {
                 n[source[i].viewName] = true;
                 target.push(source[i]);
             }
         }
         return target;
-    };
+    }
 
 }]);
